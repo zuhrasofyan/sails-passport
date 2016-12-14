1b7c07b96a2a27a14616060563530dd2712143d8
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-12-14 23:10:42 +0700

    ...Use mySql server instead of blueprints

--- config/connections.js
+++ config/connections.js
@@ -40,13 +40,13 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
-  // },
+  mysqlServer: {
+    adapter: 'sails-mysql',
+    host: 'localhost',
+    user: 'sails_passport', //optional
+    password: 'sails_passport', //optional
+    database: 'sails_passport' //optional
+  },
 
   /***************************************************************************
   *                                                                          *
